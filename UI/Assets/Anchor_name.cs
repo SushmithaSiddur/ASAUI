2ad8c4bb89b734ae8b5b93ad5195849a04866cb7
Varun Kumar Siddaraju <31942847+varunsiddaraju@users.noreply.github.com>
2020-03-10 22:33:00 -0700

    UI updated

--- UI/Assets/Anchor_name.cs
+++ UI/Assets/Anchor_name.cs
@@ -12,25 +12,31 @@ public class Anchor_name : MonoBehaviour
     // public Text txt;
     [HideInInspector]
     public string path;
-    
+
     void Start()
     {
-       
+        path = Application.dataPath + "/Log.txt";
+    }
+
+    private void Update()
+    {
+        var lines = File.ReadAllLines(path).Where(arg => !string.IsNullOrWhiteSpace(arg));
+        File.WriteAllLines(path, lines);
     }
 
     public void CreateText()
     {
-        path = Application.dataPath + "/Log.txt";
+
         string content = Anchorname.text;
         if (!File.Exists(path))
         {
 
             File.WriteAllText(path, "Anchorname.text");
         }
-          content ="\n"+ Anchorname.text;
+        content = "\n" + Anchorname.text;
 
         File.AppendAllText(path, content);
- 
+
     }
 
 
@@ -38,17 +44,17 @@ public class Anchor_name : MonoBehaviour
     {
 
         string ObjectToDeleteName = gameObject.name;
-        
+
         string ObjectToDeleteName_withNumber = ObjectToDeleteName.Replace("Delete ", "");
         ObjectToDeleteName = ObjectToDeleteName_withNumber.Remove(ObjectToDeleteName_withNumber.Length - 2);
 
-       string[] lines = File.ReadAllLines(gameObject.GetComponent<Load_anchor_name>().path);
+        string[] lines = File.ReadAllLines(gameObject.GetComponent<Load_anchor_name>().path);
 
 
         List<string> listOfLines = lines.OfType<string>().ToList();
         for (int i = listOfLines.Count - 1; i >= 0; i--)
         {
-      
+
             if (listOfLines[i].Contains(ObjectToDeleteName))
             {
                 listOfLines.RemoveAt(i);
@@ -61,6 +67,38 @@ public class Anchor_name : MonoBehaviour
         Destroy(gameObject);
         Destroy(GameObject.Find(ObjectToDeleteName_withNumber));
 
+        //StartCoroutine(ReloadAnchorNames());
+
+        // InvokeRepeating("ReloadAnchorNames", 2.0f, 0f);
+
+        ClearAnchorLocationsPage();
+        ReloadAnchorNames();
+
+
     }
 
+    public void  ReloadAnchorNames()
+    {
+        Debug.Log(1);
+        //yield return new WaitForSeconds(1f);
+        Debug.Log(2);
+        Debug.Log(GameObject.Find("Locations Page"));
+        GameObject.Find("Locations Page").GetComponent<Load_anchor_name>().loadAnchors();
+
+    }
+
+    public void ClearAnchorLocationsPage()
+    {
+        Debug.Log("something");
+
+        GameObject[] uiElement = GameObject.FindGameObjectsWithTag("ClearUI");
+        foreach (GameObject ui in uiElement)
+        {
+            Debug.Log(ui);
+            Destroy(ui);
+        }
+
+    }
+
+   
 }
--- UI/Assets/Load_anchor_name.cs
+++ UI/Assets/Load_anchor_name.cs
@@ -31,6 +31,7 @@ public class Load_anchor_name : MonoBehaviour
 
     public void loadAnchors()
     {
+        Debug.Log("Loading anchor details...");
         StreamReader reader = new StreamReader(path);
         string text;
 
@@ -76,18 +77,19 @@ public class Load_anchor_name : MonoBehaviour
     IEnumerator SetButtonPossition(string anchor, string delete1)
     {
         yield return new WaitForSeconds(0.01f);
-       
-        Vector3 AnchorButtonPosition = new Vector3(-100, 100-70* (Int32.Parse(anchor.Substring(anchor.Length - 1))-2), 0);
-       
-        Vector3 DeleteButtonPosition = new Vector3(100, 100 - 70 * (Int32.Parse(anchor.Substring(anchor.Length - 1)) - 2), 0);
-        GameObject.Find(anchor).GetComponent<RectTransform>().anchoredPosition = AnchorButtonPosition;
-        GameObject.Find(delete1).GetComponent<RectTransform>().anchoredPosition = DeleteButtonPosition;
+
+            Vector3 AnchorButtonPosition = new Vector3(-100, 100 - 70 * (Int32.Parse(anchor.Substring(anchor.Length - 1)) - 2), 0);
+
+            Vector3 DeleteButtonPosition = new Vector3(100, 100 - 70 * (Int32.Parse(anchor.Substring(anchor.Length - 1)) - 2), 0);
+            GameObject.Find(anchor).GetComponent<RectTransform>().anchoredPosition = AnchorButtonPosition;
+            GameObject.Find(delete1).GetComponent<RectTransform>().anchoredPosition = DeleteButtonPosition;
+      
+          
     }
+
     private void Update()
     {
         
     }
-
-
 
 }
